7db154d9de06436f4a317836df329701c30cf8bb
54rtor1 <1929sartori@gmail.com>
2023-10-05 23:17:48 +0200

    change effect - cursor looping between aray of images

--- components/ImageGrid.jsx
+++ components/ImageGrid.jsx
@@ -1,38 +1,60 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import imageUrls from './imageData'; // Adjust the path accordingly
 
-const ImageGrid = () => {
-  const handlePointerMove = (event, index) => {
-    console.log(`Pointer moved over image ${index + 1}`);
+const ImageTrail = () => {
+  const [imageIndex, setImageIndex] = useState(0);
+  const [imageTrail, setImageTrail] = useState([]);
+  const containerRef = useRef(null);
+
+  const handlePointerMove = (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    setImageTrail((prevTrail) => [
+      ...prevTrail,
+      {
+        top: mouseY - 25, // Adjust as needed
+        left: mouseX - 25, // Adjust as needed
+      },
+    ]);
+
+    // Increment the image index to display the next image
+    setImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
   };
 
-  const imageUrls = [
-    '../seaImages/11.png','../seaImages/2.png', '../seaImages/3.png', '../seaImages/4.png', '../seaImages/14.png', '../seaImages/6.png','../seaImages/1.png', '../seaImages/17.png','../seaImages/18.png','../seaImages/10.png', '../seaImages/12.png', '../seaImages/20.png','../seaImages/8.png','../seaImages/7.png','../seaImages/9.png',
-  ];
+  useEffect(() => {
+    // Clear the trail after a short delay
+    const timeoutId = setTimeout(() => {
+      setImageTrail([]);
+    }, 1000); // Adjust as needed
+
+    return () => clearTimeout(timeoutId);
+  }, [imageTrail]);
 
   return (
-    <div className="grid grid-cols-5 grid-rows-3 gap-4 h-screen absolute inset-0 overflow-hidden">
-      {imageUrls.map((imageUrl, index) => {
-        const animationDuration = Math.random() * 2 + 1;
-        const animationDelay = Math.random() * 2;
-
-        return (
-          <div
-            key={index}
-            className="relative overflow-hidden group animate-float brightness-100 saturation-200 opacity-6"
-            style={{ animationDuration: `${animationDuration}s`, animationDelay: `${animationDelay}s` }}
-            onMouseMove={(event) => handlePointerMove(event, index)}
-          >
-            <div className="aspect-w-1 aspect-h-1">
-              <img
-                src={imageUrl}
-                className="object-cover w-full h-full"
-              />
-            </div>
+    <div
+      ref={containerRef}
+      className="absolute inset-0 overflow-hidden"
+      onMouseMove={handlePointerMove}
+    >
+      {imageTrail.map((position, index) => (
+        <div
+          key={index}
+          className="absolute w-50 h-50 transform transition-transform duration-300"
+          style={{ top: position.top, left: position.left }}
+        >
+          <div className="aspect-w-1 aspect-h-1">
+            {/* Use the imported imageUrls array */}
+            <img
+              src={imageUrls[imageIndex]}
+              alt={`Image ${index}`}
+              className="object-cover w-full h-full"
+            />
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 };
 
-export default ImageGrid;
+export default ImageTrail;
---
+++ components/imageData.js
@@ -0,0 +1,5 @@
+const imageUrls = [
+  '../seaImages/11.png','../seaImages/2.png', '../seaImages/3.png', '../seaImages/4.png', '../seaImages/14.png', '../seaImages/6.png','../seaImages/1.png', '../seaImages/17.png','../seaImages/18.png','../seaImages/10.png', '../seaImages/12.png', '../seaImages/20.png','../seaImages/8.png','../seaImages/7.png','../seaImages/9.png',
+];
+
+export default imageUrls;
