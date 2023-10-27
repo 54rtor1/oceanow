183d1bd1da61f87f30768b9b588eb2ed8081d5c7
54rtor1 <1929sartori@gmail.com>
2023-10-28 00:26:22 +0200

    animation updateed

--- components/ui/AnimationSketch.jsx
+++ components/ui/AnimationSketch.jsx
@@ -1,3 +1,17 @@
+// Easing function for smoothing the transition
+function cubicEaseInOut(t) {
+  return t < 0.5 ? 4 * t ** 3 : 1 - (-2 * t + 2) ** 3 / 2;
+}
+
+// Extended easing function for values from 0 to 3
+function extendedCubicEaseInOut(t) {
+  if (t <= 1) {
+    return cubicEaseInOut(t);
+  } else {
+    return map(t, 1, 3, 1, cubicEaseInOut(1));
+  }
+}
+
 const AnimationSketch = (p) => {
   let waveHeight;
   let time = 0;
@@ -16,11 +30,17 @@ const AnimationSketch = (p) => {
     const canvasWidth = 800;
     const canvasHeight = 390;
     p.createCanvas(canvasWidth, canvasHeight, p.WEBGL); // Use WEBGL renderer
-    waveHeight = 200; // Initial value, will be updated dynamically
-    seaFillHeight = waveHeight; // Initialize seaFillHeight
+    let APIvalue = 2; // to be dynamically updated
+    let initialSeaHeight = APIvalue / 3; // adjusts value from API
+    waveHeight = p.map(extendedCubicEaseInOut(initialSeaHeight), 0, 1, 10, 250); // Apply easing for smoothing
+    waveDirection = 1; // Initialize waveDirection
+
+    // Calculate seaFillHeight dynamically based on the initial sea height
+    seaFillHeight = p.map(extendedCubicEaseInOut(initialSeaHeight), 0, 1, p.height / 3, p.height / 6); // Apply easing for smoothing
+
     p.noiseDetail(1.8);
     for (let i = 0; i < p.width / 8; i++) {
-    decayedWave.push(0);
+      decayedWave.push(0);
     }
   };
 
@@ -33,64 +53,73 @@ const AnimationSketch = (p) => {
     p.background(0); // Black background
 
     // Calculate the vertical offset based on waveHeight
-    let verticalOffset = p.map(waveHeight, 0, 100, 0, p.height);
+    let canvasHeight = p.height; // Store canvas height
+    let verticalOffset = p.map(waveHeight, 0, 100, 0, canvasHeight);
 
     // Set up lighting
-    p.directionalLight(255, 255, 255, 0, 1, -1);
+    p.directionalLight(227, 144, 54, -0, -1, -1); // Cor da Luz
 
     // Create a sunset-colored background using a large rectangle
     p.push();
     p.translate(0, 0, -10); // Move the rectangle behind the wave
-    p.specularMaterial(255, 100, 0); // Sunset orange
+    p.specularMaterial(252, 121, 228); // Cor do Fundo
     p.noStroke();
     p.rectMode(p.CENTER); // Draw the rectangle from the center
-    p.rect(0, 0, p.width * 2, p.height * 2); // Make the rectangle larger to cover the canvas
+    p.rect(0, 0, p.width * 2, canvasHeight * 2); // Make the rectangle larger to cover the canvas
     p.pop();
+    p.specularMaterial(227, 144, 54); // Cor da Onda
 
     // wave
     p.push();
-    p.translate(0, seaFillHeight / 6, 0); // Adjust for 3D space
+    p.translate(0, seaFillHeight, 0); // Adjust for 3D space
 
     p.noStroke();
-    p.specularMaterial(50, 100, 255); // Color for the wave
 
-    p.beginShape(p.TRIANGLE_STRIP); // Use TRIANGLE_STRIP to create a filled shape
+    p.beginShape(p.TRIANGLE_STRIP); // Use QUADS to create a filled shape
 
-    for (let x = -p.width / 2; x < p.width / 2 + 10; x += 10) {
-      let noiseOffset = p.map(x, -p.width / 2, p.width / 2, 0, 8);
+    for (let x = -p.width / 2; x < p.width / 2 + 10; x += 10) { // Adjust for 3D space and full width
+      let noiseOffset = p.map(x, -p.width / 2, p.width / 2, 0, 8); // Adjust for 3D space and full width
       let originalY = p.map(p.noise(noiseOffset, time), 0, 1, -waveHeight, waveHeight);
 
-      let speedModifier = 1 + (0.5 * waveDirection * p.cos(p.map(x, -p.width / 2, p.width / 2, 0, p.PI)));
+      // Apply speed modulation based on wave direction
+      let speedModifier = 1 + (0.5 * waveDirection * p.cos(p.map(x, -p.width / 2, p.width / 2, 0, p.PI))); // Adjust for 3D space and full width
       originalY *= speedModifier;
 
-      let decayedY = decayedWave[p.int((x + p.width / 2) / 10)] * decayFactor;
+      let decayedY = decayedWave[p.int((x + p.width / 2) / 10)] * decayFactor; // Adjust index for full width
       let y = decayedY;
 
-      if (x + p.width / 2 > wavePosition) {
+      // Apply the influence of the wave
+      if (x + p.width / 2 > wavePosition) { // Adjust for full width
         y += p.map(p.sin((x - wavePosition) * waveSpeed), -1, 1, 0, waveAmplitude);
       }
 
+      // Add vertices to the shape
       p.vertex(x, y, 0);
-      p.vertex(x, y + seaFillHeight, 0);
+      p.vertex(x, canvasHeight / 2, 0); // Extend vertices to the bottom of the canvas
 
-      decayedWave[p.int((x + p.width / 2) / 10)] = originalY;
-      let targetDirection = animationPhase === 0 ? -1 : 1;
+      decayedWave[p.int((x + p.width / 2) / 10)] = originalY; // Adjust index for full width
+
+      // Smoothly transition animation direction and phase
+      let targetDirection = extendedCubicEaseInOut(animationPhase) === 0 ? -1 : 1;
       waveDirection = p.lerp(waveDirection, targetDirection, directionTransitionSpeed);
       animationPhase = p.lerp(animationPhase, targetDirection, directionTransitionSpeed);
+
+      // Ensure animation phase stays within limits
       animationPhase = p.constrain(animationPhase, 0, 1);
     }
 
     p.endShape(p.CLOSE);
 
-    p.pop();
+    p.pop(); // Restore the transformation matrix
 
+    // Update wave position
     wavePosition += animationSpeed * waveAmplitude * waveDirection;
     if (wavePosition > p.width / 2) {
-      wavePosition = -p.width / 2;
+      wavePosition = -p.width / 2; // Reset the wave position
     }
 
     time += animationSpeed;
   };
-}
+};
 
 export default AnimationSketch;
